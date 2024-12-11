7b67f2599139301dcb341b53cfc30a5e840b76a1
Tania <nadsanp@gmail.com>
2024-12-11 12:22:35 +0100

    start fetch

--- web/src/components/Projects.jsx
+++ web/src/components/Projects.jsx
@@ -1,26 +1,31 @@
-import Header from './Header';
-import Hero from './Hero';
-import Preview from './Preview';
+import Header from "./Header";
+import Hero from "./Hero";
+import Preview from "./Preview";
 import "../scss/components/Projects.scss";
-import Footer from './Footer';
-
+import Footer from "./Footer";
+import { useEffect } from "react";
 
 function Projects() {
-return (
-    
-    <div className="projectColor">
-        <Header />
-        <Hero />
-        <div className="previewGrid">
-            <Preview />
-            <Preview />
-            <Preview />
-            <Preview />
-        </div>
-        <Footer />
-    </div>
+  useEffect(() => {
+    fetch("http://localhost:3001/projects/list").then((res) =>
+      res.json().then((data) => {
+        const dataProjects = data.message;
+      })
+    );
+  }, []);
 
-    
-)
+  return (
+    <div className="projectColor">
+      <Header />
+      <Hero />
+      <div className="previewGrid">
+        <Preview />
+        <Preview />
+        <Preview />
+        <Preview />
+      </div>
+      <Footer />
+    </div>
+  );
 }
-export default Projects
+export default Projects;
