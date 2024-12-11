cbe4839dcf9daf9334315cff16bc07b445fb92a3
Tania <nadsanp@gmail.com>
2024-12-11 12:25:23 +0100

    start fetch

--- src/index.js
+++ src/index.js
@@ -1,21 +1,46 @@
 const express = require("express");
-const cors = require ("cors");
+const cors = require("cors");
 
 //crear el servidor con express
 
-const server = express ();
+const server = express();
 
 // permitir peticiones
 
 server.use(cors());
 
 // correr el servidor en un puerto
-
+const projects = [{
+    "name": "BookLinker",
+    "slogan": "Conecta lectores con historias inolvidables",
+    "technologies": "JavaScript, React, Firebase",
+    "repo": "https://github.com/autora/booklinker",
+    "demo": "https://booklinker.demo.com",
+    "desc": "Una plataforma que recomienda libros a los usuarios según sus lecturas pasadas y les permite registrar su progreso de lectura.",
+    "job": "Desarrolladora Full Stack"
+},
+{
+    "name": "InvitePro",
+    "slogan": "Tus eventos, organizados y únicos",
+    "technologies": "HTML, CSS, JavaScript, Node.js",
+    "repo": "https://github.com/autora/invitepro",
+    "demo": "https://invitepro.demo.com",
+    "desc": "Un sitio web para crear invitaciones digitales personalizadas para eventos sociales y corporativos, con opciones para compartir fácilmente en redes.",
+    "job": "Diseñadora de Interfaces y Desarrolladora Web"
+}
+];
+server.get("/projects/list", (req, res) => {
+    res.status(200).json({
+        success: true,
+        message: projects
+    })
+})
 
 const port = 3001;
 server.listen(port, () => {
-    console.log(`server is running in http://localhost:${port}` )
+    console.log(`server is running in http://localhost:${port}`)
 })
 
 const staticServerPath = "./web/dist";
 server.use(express.static(staticServerPath));
+
